11aaaa6c8e6eeb5affe1005db429974478e64b6c
Ilia <hello@ilija.ru>
2020-01-29 23:34:33 +0300

    test API

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import axios from 'axios';
 
 export const Form = props => {
+    const handleLocationChange = e => {
+        const value = e.target.value;
+        axios({
+            method: 'get',
+            url: `https://api.openweathermap.org/data/2.5/weather?q=${value}&APPID=b747504d35c2c18115f8d1b1d4389f02`
+        })
+        .then(resp => {
+            console.log(resp);
+            console.log(value);
+        })
+        .catch(error => {
+            alert(error);
+        })
+    }
+
     return (
         <div>
-            <input type='text' />
+            <input type='text' onChange={handleLocationChange} />
             <input type='button' value='+' />
         </div>
     );

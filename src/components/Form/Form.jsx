e65f63af4cd075b499e4b88f672dae32d564cefc
Ilia <hello@ilija.ru>
2020-01-29 21:11:30 +0300

    initial commit + form component

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Form = props => {
+    return (
+        <div>
+            <input type='text' />
+            <input type='button' value='+' />
+        </div>
+    );
+}
